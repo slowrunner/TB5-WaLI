d6c6873aae69aad4fa75a53ae41c251b1084d7a2
slowrunner <slowrunner@users.noreply.github.com>
2025-01-12 00:00:00 -0500

    latest

--- config/download_and_run_turtlebot4_setup.sh
+++ config/download_and_run_turtlebot4_setup.sh
@@ -7,5 +7,7 @@ wget -qO - https://raw.githubusercontent.com/turtlebot/turtlebot4_setup/jazzy/sc
 
 #
 echo -e "Be sure to add RTC and USB Current mods to /boot/firmware/config.txt now"
+echo -e "Add ipv6.disable=1 to /boot/firmware/cmdline.txt now"
+echo -e "Check /etc/netplan/50-cloud-init.yaml for WiFi setup"
 echo -e "then reboot"
 echo -e "then run: turtlebot4-setup"
