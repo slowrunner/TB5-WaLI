e025e3f6b7320b6869a11fa9c61fdcec698f2402
slowrunner <slowrunner@users.noreply.github.com>
2025-01-12 14:19:55 -0500

    cleaning

--- wali_ws/ss.sh
+++ wali_ws/ss.sh
@@ -15,13 +15,4 @@ if [ -f ~/TB5-WaLI/dai_ws/install/setup.bash ]; then
     echo -e "sourced dai_ws install setup.bash"
 fi
 
-export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-# export FASTRTPS_DEFAULT_PROFILES_FILE=/home/pi/wali_pi5/configs/super_client_configuration_file.xml
-export ROS_DISCOVERY_SERVER=127.0.0.1:11811
-
-# Set to only communicate via the republisher
-# export FASTRTPS_DEFAULT_PROFILES_FILE=~/wali_pi5/configs/fastdds-passive-unicast.xml
-# ros2 daemon stop
-
-# ros2 topic list
-# ros2 topic list
+echo -e "Done\n"
