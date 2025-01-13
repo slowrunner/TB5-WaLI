c8983983cc2111b7cf3d3ed9081a17758a892a13
slowrunner <slowrunner@users.noreply.github.com>
2025-01-13 13:37:14 -0500

    C1 Lidar working with turtlebot4_bringup

---
+++ wali_ws/src/sllidar_ros2/scripts/turtlebot4_c1_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo cp install/sllidar_ros2/share/sllidar_ros2/launch/rplidar.launch.py /opt/ros/jazzy/share/turtlebot4_bringup/launch/
+sudo cp install/sllidar_ros2/lib/sllidar_ros2/sllidar_node /opt/ros/jazzy/lib/rplidar_ros/
