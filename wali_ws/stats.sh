ead3661075db2f4b57c427156ec7aa2932de6e66
slowrunner <slowrunner@users.noreply.github.com>
2025-01-16 19:31:38 -0500

    update stats

--- wali_ws/stats.sh
+++ wali_ws/stats.sh
@@ -3,3 +3,6 @@
 ~/TB5-WaLI/utils/totallife.sh
 echo -e "\nLast Dock and UnDock:"
 tail ~/TB5-WaLI/logs/life.log | grep -E 'Docking: success | Undocking: success'
+batt=$(    ros2 topic echo --once --field percentage /battery_state)
+batt="${batt:0:4}"
+echo "Battery: $batt"
