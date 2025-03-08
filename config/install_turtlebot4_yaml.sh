ab0769b6b778a898e5521673c0410df64042154a
slowrunner <slowrunner@users.noreply.github.com>
2025-03-08 08:43:14 -0500

    fix destination

--- config/install_turtlebot4_yaml.sh
+++ config/install_turtlebot4_yaml.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo -e "sudo cp ~/TB5-WaLI/config/turtlebot4.urdf.xacro /opt/ros/jazzy/share/turtlebot4_description/urdf/lite/"
-# sudo cp ~/TB5-WaLI/config/turtlebot4.yaml /opt/ros/jazzy/share/turtlebot4_bringup/config/turtlebot4.yaml
-cp ~/TB5-WaLI/config/turtlebot4.yaml ~/TB5-WaLI/wali_ws/src/turtlebot4_fork_for_TB5-WaLI/turtlebot4_bringup/config/
+echo -e "cp ~/TB5-WaLI/config/turtlebot4.yaml ~/TB5-WaLI/wali_s/src/turtlebot4_robot_fork_for_TB5-WaLI/turtlebot4_bringup/config/"
+cp ~/TB5-WaLI/config/turtlebot4.yaml ~/TB5-WaLI/wali_ws/src/turtlebot4_robot_fork_for_TB5-WaLI/turtlebot4_bringup/config/
