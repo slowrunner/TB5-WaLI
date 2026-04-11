1d7d0461705338a561d5978d79b390ba98385e19
slowrunner <slowrunner@users.noreply.github.com>
2026-04-10 21:08:31 -0400

    working keepout filter

--- wali_ws/cmds/launch_test_costmap_filter.sh
+++ wali_ws/cmds/launch_test_costmap_filter.sh
@@ -4,4 +4,4 @@
 
 ros2 launch wali costmap_filter_info.launch.py \
    keepout_map:=/home/ubuntu/TB5-WaLI/wali_ws/maps/tb5wali_current.keepout.yaml \
-   keepout_params_file:=/home/ubuntu/TB5-WaLI/wali_ws/params/costmap_fileter_keepout_params.yaml
+   keepout_params_file:=/home/ubuntu/TB5-WaLI/wali_ws/params/costmap_filter_keepout_params.yaml
